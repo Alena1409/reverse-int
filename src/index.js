2d1a55f785fa12d588bb4e390ad9e832631f86ac
Alena1409 <a77993519@gmail.com>
2024-07-29 19:54:29 +0400

    init: start task revers int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let a = String(n);
+
+    if (a[0] == '-') {
+        i = 1;
+        while (i < a.length) {
+            result = a[i] + result;
+            i = i + 1;
+        }
+    } else {
+        i = 0;
+        while (i < a.length){
+            result = a[i] + result;
+            i = i + 1;
+        }
+    }
+    return result;
 }
